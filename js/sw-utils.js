c51617f76b30d54a2290a6e1d16a65b523d54e87
AHMarcos <marcos.alarcon@vallegrande.edu.pe>
2020-05-22 15:57:36 -0500

    subiendo al reposi

---
+++ js/sw-utils.js
@@ -0,0 +1,13 @@
+
+function actualizarCacheDinamico (dynamicCache, req, resp){
+    
+    if (resp.ok) {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, resp.clone());
+                return resp.clone();
+        });
+    }else{
+        return resp;
+    }
+
+}
